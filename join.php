513eb7e6b946f96a625d32b98bd9d6b5ddc71488
hun24 <100418580+hun24@users.noreply.github.com>
2022-07-31 16:35:58 +0900

    Update join.php

--- join.php
+++ join.php
@@ -4,6 +4,16 @@
     <head>
         <meta charset="UTF-8">
         <title>Join</title>
+        function checkid(){
+	        var userid = document.getElementById("uid").value;
+	        if(userid)  //userid로 받음
+	        {
+		        url = "check.php?userid="+userid;
+		        window.open(url,"chkid","width=400,height=200");
+	        } else {
+		        alert("아이디를 입력하세요.");
+	        }
+        }
     </head>
     <body>
         <h2>회원가입</h2>
